9079c2c93fa232a8d7ae527c7190da19d6921bb4
zkula <zachkula@hotmail.com>
2020-09-14 15:12:12 -0400

    Reset Quiz Functionality

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,6 +13,10 @@ class Quiz extends Component {
         };
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1})
+    }
+    
     showNextQuestion () {
         this.setState({
             quiz_position: this.state.quiz_position + 1
@@ -24,7 +28,7 @@ class Quiz extends Component {
       
         return (
         <div className="QuizQuestion">
-            {isQuizEnd ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>}
+            {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion showNextQuestionHandler = {this.showNextQuestion.bind(this)} quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>}
         </div>
         
       )
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
+    handleResetClick(){
+        this.props.resetClickHandler();
+    }
+    
     render() {
       return (
         <div>
         <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
+        <a onClick={this.handleResetClick.bind(this)} href=''>Reset Quiz</a>
         </div>
       
         );
